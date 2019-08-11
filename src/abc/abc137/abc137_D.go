88ebd5745095040bc5e18e8429bd3d1b9298076c
cscs8 <42040439+cscs8@users.noreply.github.com>
2019-08-11 22:02:37 +0900

    comp -no comment

--- src/abc/abc137/abc137_D.go
+++ src/abc/abc137/abc137_D.go
@@ -35,10 +35,6 @@ func TestD() {
 
 		jobList[i] = Job{a, b, cost}
 		fmt.Println("cost : ", cost)
-
-		// if retMap[a] < b {
-		// 	retMap[a] = b
-		// }
 	}
 
 	// DESC JobList sort
@@ -48,28 +44,14 @@ func TestD() {
 	dayCost := 0
 	days := m
 	ret := 0
-	sum := 0
 	i := 0
 	fmt.Println("maxDayCost : ", maxDayCost)
-	// for _, j := range jobList {
 	size := len(jobList)
 	if m < len(jobList) {
 		size = m
 	}
 	for _, j := range jobList[:size] {
-		fmt.Println()
-		fmt.Println("job : ", j)
-		fmt.Println("maxDayCost : ", maxDayCost)
-		fmt.Println("dayCost : ", dayCost)
-		fmt.Println("j.A : ", j.A)
-		fmt.Println("days : ", days)
-		fmt.Println("sum : ", sum)
-		// if (maxDayCost < j.A) || (days < j.A) {
-		// if days < j.A {
 		if maxDayCost < j.A || maxDayCost < i+j.A {
-			fmt.Println("dayCost+j.A : ", dayCost+j.A)
-			fmt.Println("Continue!")
-			fmt.Println()
 			i++
 			maxDayCost -= j.A
 			days--
@@ -78,34 +60,10 @@ func TestD() {
 
 		dayCost += j.A
 		ret += j.B
-		fmt.Println("ret : ", ret)
 		maxDayCost -= j.A
 		days--
 		i++
 	}
 
-	fmt.Println("jobList : ", jobList)
 	fmt.Println(ret)
-	// var sc = bufio.NewScanner(os.Stdin)
-	// if sc.Scan() {
-	// 	n, _ = strconv.Atoi(sc.Text())
-	// }
-	// var list = make([]string, n)
-	// for i := 0; i < n; i++ {
-	// 	if sc.Scan() {
-	// 		list[i] = sc.Text()
-	// 	}
-	// }
-
-	// m := map[string]int{}
-	// for _, s := range list {
-	// 	m[SortStringByCharacter(s)]++
-	// }
-
-	// ret := 0
-	// for _, v := range m {
-	// 	ret += v * (v - 1) / 2
-	// }
-
-	// fmt.Println(ret)
 }
