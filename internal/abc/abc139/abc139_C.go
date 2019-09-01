7974d181fb7a3e298e399f592493fe4d3d716f47
cscs8 <softcream.ice31@gmail.com>
2019-09-01 22:17:53 +0900

    comment on

--- internal/abc/abc139/abc139_C.go
+++ internal/abc/abc139/abc139_C.go
@@ -15,26 +15,32 @@ func TestC() {
 
 	var sc = bufio.NewScanner(os.Stdin)
 	var list = make([]string, n)
-	var listInt = make([]int, n)
+	var listInt = make([]int64, n)
 
 	if sc.Scan() {
 		list = strings.Fields(sc.Text())
 	}
 
 	for idx, value := range list {
-		valueInt, _ := strconv.Atoi(value)
+		// valueInt, _ := strconv.Atoi(value)
+		valueInt, _ := strconv.ParseInt(value, 10, 64)
 		listInt[idx] = valueInt
 	}
 
 	max, tmp := 0, 0
 	fmt.Println(listInt)
 	for i := 0; i < n-1; i++ {
-		fmt.Println("list[i", i, "] : ", list[i])
+		if 0 >= n-max {
+			break
+		}
+		// tmp = 0
+		j := i + tmp
+		if i+1 > j {
+			j = i + 1
+		}
 		tmp = 0
-
-		for j := i + 1; j < n; j++ {
-			fmt.Println("  list[j:", j, "] : ", list[j])
-			fmt.Println("  list[j-1", j-1, "] : ", list[j-1])
+		for ; j < n; j++ {
+			fmt.Println("j : ", j)
 			if listInt[j] > listInt[j-1] {
 				break
 			}
