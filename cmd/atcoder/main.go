39bfd0773d4d645c45aa8df4e5c33d71e3e95777
cscs8 <softcream.ice31@gmail.com>
2019-12-08 23:08:33 +0900

    abc147

--- cmd/atcoder/main.go
+++ cmd/atcoder/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/cscs8/atcoder/internal/abc/abc146"
+	"github.com/cscs8/atcoder/internal/abc/abc147"
 )
 
 func main() {
 	fmt.Printf("hello, world!\n")
-	abc146.TestC()
+	abc147.TestC()
 }
